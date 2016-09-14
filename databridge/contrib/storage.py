613b041fb13d26376b5ce93b48ee3c3f178c7989
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2016-09-14 17:31:02 +0300

    Add couchdb storage

---
+++ databridge/contrib/storage.py
@@ -0,0 +1,27 @@
+import couchdbreq
+import requests
+from requests.adapters import HTTPAdapter
+from databridge.helpers import create_db_url
+
+
+class Storage(object):
+
+    def __init__(self, config, session=None, adapter=None):
+        if not isinstance(config, dict):
+            raise TypeError(
+                "Expected a dict as config, got {}".format(type(config))
+            )
+
+        server_url = create_db_url(
+            config.get('username', ''),
+            config.get('password', ''),
+            config.get('host'),
+            config.get('port')
+        )
+        if not session:
+            session = requests.Session()
+        if not adapter:
+            adapter = HTTPAdapter(pool_maxsize=50, pool_connections=100)
+        server = couchdbreq.Server(server_url, session=session)
+        session.mount(server_url, adapter)
+        self.db = server.get_or_create_db(config.get('db_name', 'tenders_db'))

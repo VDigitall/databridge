e51df6344a79d20ef5b53a67c772d416c80396e8
Yaroslav Shalenyk <yshalenyk@gmail.com>
2016-09-15 01:21:13 +0300

    Modify workers

--- databridge/contrib/workers.py
+++ databridge/contrib/workers.py
@@ -1,6 +1,10 @@
 import gevent
+from gevent.pool import Pool
+import logging
 from .client import APICLient
 from .storage import Storage
+
+logger = logging.getLogger(__name__)
 
 
 class Fetch(gevent.Greenlet):
@@ -8,22 +12,24 @@ class Fetch(gevent.Greenlet):
     def __init__(self, src_queue, dest_queue, config):
 
         super(Fetch, self).__init__()
+        session = config.get('session', None)
         self.client = APICLient(
-            config.get('api_key'),
+            config.get('api_key', ''),
             config.get('api_host'),
-            config.get('api_version')
+            config.get('api_version'),
+            session=session
         )
-        if callable(src_queue):
-            self.source = src_queue
-        else:
-            self.source = src_queue.get
+        self.source = src_queue
         self.dest = dest_queue
 
     def _run(self):
         while True:
-            for feed in self.source():
-                r = self.client.fetch(feed)
-                self.dest.put(r)
+            for feed in self.source:
+                resp = self.client.fetch(feed)
+                #for tender in feed:
+                #    resp = self.client.get_tender(tender['id'])
+                self.dest.put(resp)
+                gevent.sleep(2)
             gevent.sleep(2)
 
 
@@ -33,24 +39,20 @@ class Save(gevent.Greenlet):
 
         super(Save, self).__init__()
         self.storage = config.get('storage', Storage(config))
-
-        if callable(src_queue):
-            self.source = src_queue
-        else:
-            self.source = src_queue.get
+        self.source = src_queue
         self.dest = dest_queue
 
     def _run(self):
         while True:
-            tenders = self.source()
-            for tender in tenders:
-                tender['_id'] = tender['id']
-                if tender['_id'] in self.storage.db:
-                    self.dest.put('Update doc; {}'.format(tender['id']))
-                    doc = self.storage.db.get_doc(tender['_id'])
-                    doc = tender
-                    self.storage.db.save_doc(doc, batch=True)
-                else:
-                    self.dest.put('Save doc; {}'.format(tender['id']))
-                    self.storage.db.save_doc(tender)
+            for feed in self.source:
+                for tender in feed:
+                    tender['_id'] = tender['id']
+                    if tender['_id'] in self.storage.db:
+                        self.dest.put('Update doc; {}'.format(tender['id']))
+                        doc = self.storage.db.get_doc(tender['_id'])
+                        doc = tender
+                        self.storage.db.save_doc(doc, batch=True)
+                    else:
+                        self.dest.put('Save doc; {}'.format(tender['id']))
+                        self.storage.db.save_doc(tender)
         gevent.sleep(2)

194b9f48f31122480dd7699c08e800844afedac6
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2016-09-15 19:09:22 +0300

    Fetch feed page with grequests

--- databridge/contrib/client.py
+++ databridge/contrib/client.py
@@ -2,12 +2,11 @@ import requests
 from requests.adapters import HTTPAdapter
 import grequests
 import logging
-from databridge.helpers import RetryAdapter
+from databridge.helpers import APIAdapter
 from ujson import loads
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
 
 
 class APICLient(object):
@@ -16,17 +15,13 @@ class APICLient(object):
 
         self.base_url = "{}/api/{}".format(api_host, api_version)
 
-        if 'session' in options:
-            self.session = options.pop('session')
-        else:
-            self.session = requests.Session()
+        self.session = requests.Session()
         self.session.auth = (api_key, '')
         self.session.headers = {"Accept": "applicaiton/json",
-                                    "Content-type": "application/json"}
+                                "Content-type": "application/json"}
         resourse = options.get('resourse', 'tenders')
         self.resourse_url = "{}/{}".format(self.base_url, resourse)
-        #self.session.mount(self.resourse_url, RetryAdapter)
-        self.session.mount(self.resourse_url, HTTPAdapter(pool_connections=15, pool_maxsize=5))
+        self.session.mount(self.resourse_url, APIAdapter)
 
         # retrieve cookie
         self.session.head("{}/{}".format(self.base_url, 'spore'))
@@ -34,16 +29,20 @@ class APICLient(object):
     def get_tenders(self, params=None):
         if not params:
             params = {'feed': 'chages'}
-        resp = self.session.get(self.resourse_url, params=params)
-        if resp.ok:
-            resp.close()
-            return resp.json()
-        else:
-            logger.warn(
-                'Fail while fetching tenders '
-                'feed with client params: {}'.format(params)
-            )
-            resp.raise_for_status()
+        req = [grequests.get(self.resourse_url,
+                             params=params,
+                             session=self.session)]
+
+        resp = grequests.map(req)
+        for r in resp:
+            if r.ok:
+                return r.json()
+            else:
+                logger.warn(
+                    'Fail while fetching tenders '
+                    'feed with client params: {}'.format(params)
+                )
+                r.raise_for_status()
 
     def get_tender(self, tender_id, params=None):
         resp = self.session.get(
@@ -57,8 +56,8 @@ class APICLient(object):
     def fetch(self, tender_ids):
         urls = ['{}/{}'.format(self.resourse_url, tender_id['id'])
                 for tender_id in tender_ids]
-        resp = (grequests.request('GET', url, session=self.session, stream=False)
+        resp = (grequests.get(url, session=self.session, stream=False)
                 for url in urls)
-        results = [t.json()['data'] for t in grequests.map(resp, size=5)]
+        results = [t.json()['data'] for t in grequests.map(resp, size=10)]
         [r.close() for r in resp]
         return results
